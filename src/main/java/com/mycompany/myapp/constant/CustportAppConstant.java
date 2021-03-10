913d77029eb5d8b90a6318093c952bd227eed2cb
claire-min <claire.min@veriday.com>
2021-03-10 14:33:55 -0500

    integration for product version

--- src/main/java/com/mycompany/myapp/constant/CustportAppConstant.java
+++ src/main/java/com/mycompany/myapp/constant/CustportAppConstant.java
@@ -2,4 +2,5 @@ package com.mycompany.myapp.constant;
 
 public class CustportAppConstant {
 	public static final String DATE_FORMAT = "MMM yyyy";
+	public static final String INPUT_DATE_FORMAT = "MM/dd/yyyy";
 }
