39027254b7a64e7c3cc75f449b3efba2751eefa2
Adam Adjindji <adam.adjindji@veriday.com>
2021-03-16 13:44:16 -0400

    formatting

--- ui/widgets/cp-widgets-dir/cp-widgets/src/components/Admin/ManageUser/AssignUser.js
+++ ui/widgets/cp-widgets-dir/cp-widgets/src/components/Admin/ManageUser/AssignUser.js
@@ -6,155 +6,155 @@ import withKeycloak from '../../../auth/withKeycloak';
 import { apiKeycloakUserGet } from '../../../api/keycloak';
 import i18n from '../../../i18n';
 class AssignUser extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projectId: '',
-      assignUser: '',
-      users: new Map(),
-      projects: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            projectId: '',
+            assignUser: '',
+            users: new Map(),
+            projects: {}
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    async fetchData() {
+        const { t, keycloak } = this.props;
+
+        const authenticated = keycloak.initialized && keycloak.authenticated;
+        if (authenticated) {
+            const users = this.mapKeycloakUserEmails((await apiKeycloakUserGet(this.props.keycloakUrl)).data);
+            const projects = (await apiGetProjectIdNames(this.props.serviceUrl)).data;
+            this.setState({
+                users,
+                projects
+            });
+        }
+    }
+
+    mapKeycloakUserEmails = keycloakUsers => {
+        const usernameEmailMap = new Map();
+        keycloakUsers.forEach(keycloakUser => usernameEmailMap.set(keycloakUser.username, keycloakUser.email));
+        return usernameEmailMap;
     };
-  }
 
-  componentDidMount() {
-    this.fetchData();
-  }
+    handleChanges = e => {
+        const input = e.target;
+        const name = input.name;
+        const value = input.value;
+        this.setState({ [name]: value });
+    };
 
-  async fetchData() {
-    const { t, keycloak } = this.props;
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const { projectId, assignUser } = this.state;
 
-    const authenticated = keycloak.initialized && keycloak.authenticated;
-    if (authenticated) {
-      const users = this.mapKeycloakUserEmails((await apiKeycloakUserGet(this.props.keycloakUrl)).data);
-      const projects = (await apiGetProjectIdNames(this.props.serviceUrl)).data;
-      this.setState({
-        users,
-        projects
-      });
-    }
-  }
+        if (projectId.length !== 0 && assignUser.length !== 0) {
+            this.assignUserToProject(projectId, assignUser);
+        }
 
-  mapKeycloakUserEmails = keycloakUsers => {
-    const usernameEmailMap = new Map();
-    keycloakUsers.forEach(keycloakUser => usernameEmailMap.set(keycloakUser.username, keycloakUser.email));
-    return usernameEmailMap;
-  };
+        // TODO: improved validation and rejection
+    };
 
-  handleChanges = e => {
-    const input = e.target;
-    const name = input.name;
-    const value = input.value;
-    this.setState({ [name]: value });
-  };
+    assignUserToProject = async (projectId, username) => {
+        const portalUserId = await this.getPortalUserId({ username, email: this.state.users.get(username) });
+        const result = await apiAddUserToProject(this.props.serviceUrl, projectId, portalUserId);
+        // TODO: Feedback if succeeded or failed.
+        this.render();
+    };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    const { projectId, assignUser } = this.state;
+    getPortalUserId = async keycloakUser => {
+        let portalUserId = null;
+        try {
+            const portalUser = await portalUserApi.apiUserGetByUsername(this.props.serviceUrl, keycloakUser.username);
+            portalUserId = portalUser.data.id;
+        } catch (e) {
+            if (e.message.toLowerCase().includes('not found')) {
+                const portalUser = await this.createPortalUser(keycloakUser);
+                portalUserId = portalUser.data.id;
+            }
+        }
 
-    if (projectId.length !== 0 && assignUser.length !== 0) {
-      this.assignUserToProject(projectId, assignUser);
-    }
+        return portalUserId;
+    };
 
-    // TODO: improved validation and rejection
-  };
+    createPortalUser = async keycloakUser => {
+        return await portalUserApi.apiUserPost(this.props.serviceUrl, { username: keycloakUser.username, email: keycloakUser.email });
+    };
 
-  assignUserToProject = async (projectId, username) => {
-    const portalUserId = await this.getPortalUserId({ username, email: this.state.users.get(username) });
-    const result = await apiAddUserToProject(this.props.serviceUrl, projectId, portalUserId);
-    // TODO: Feedback if succeeded or failed.
-    this.render();
-  };
+    setupFormComponents() {
+        const users = this.state.users;
+        const projectIdsNames = this.state.projects;
+        let userList,
+            projectList = (userList = null);
 
-  getPortalUserId = async keycloakUser => {
-    let portalUserId = null;
-    try {
-      const portalUser = await portalUserApi.apiUserGetByUsername(this.props.serviceUrl, keycloakUser.username);
-      portalUserId = portalUser.data.id;
-    } catch (e) {
-      if (e.message.toLowerCase().includes('not found')) {
-        const portalUser = await this.createPortalUser(keycloakUser);
-        portalUserId = portalUser.data.id;
-      }
-    }
+        if (users.size > 0) {
+            userList = [...users.keys()].map((assignUser, i) => (
+                <SelectItem key={i} text={assignUser} value={assignUser}>
+                    {assignUser}
+                </SelectItem>
+            ));
+            userList.unshift(<SelectItem key="-1" text={i18n.t('manageUsers.assign.assignUser')} value="" />);
+        } else {
+            userList = <SelectItem text="There are currently no users in the system" value="" />;
+        }
 
-    return portalUserId;
-  };
+        if (projectIdsNames != null && Object.keys(projectIdsNames).length > 0) {
+            projectList = Object.keys(projectIdsNames).map((projectId, i) => (
+                <SelectItem key={i} text={projectIdsNames[projectId]} value={projectId}>
+                    test
+                </SelectItem>
+            ));
+            projectList.unshift(<SelectItem key="-1" text="Project List" value="" />);
+        } else {
+            projectList = <SelectItem text="There are currently no projects in the system" value="" />;
+        }
 
-  createPortalUser = async keycloakUser => {
-    return await portalUserApi.apiUserPost(this.props.serviceUrl, { username: keycloakUser.username, email: keycloakUser.email });
-  };
-
-  setupFormComponents() {
-    const users = this.state.users;
-    const projectIdsNames = this.state.projects;
-    let userList,
-      projectList = (userList = null);
-
-    if (users.size > 0) {
-      userList = [...users.keys()].map((assignUser, i) => (
-        <SelectItem key={i} text={assignUser} value={assignUser}>
-          {assignUser}
-        </SelectItem>
-      ));
-      userList.unshift(<SelectItem key="-1" text={i18n.t('manageUsers.assign.assignUser')} value="" />);
-    } else {
-      userList = <SelectItem text="There are currently no users in the system" value="" />;
+        return { userList, projectList };
     }
 
-    if (projectIdsNames != null && Object.keys(projectIdsNames).length > 0) {
-      projectList = Object.keys(projectIdsNames).map((projectId, i) => (
-        <SelectItem key={i} text={projectIdsNames[projectId]} value={projectId}>
-          test
-        </SelectItem>
-      ));
-      projectList.unshift(<SelectItem key="-1" text="Project List" value="" />);
-    } else {
-      projectList = <SelectItem text="There are currently no projects in the system" value="" />;
-    }
+    render() {
+        const { userList, projectList } = this.setupFormComponents();
 
-    return { userList, projectList };
-  }
-
-  render() {
-    const { userList, projectList } = this.setupFormComponents();
-
-    return (
-      <div>
-        <Form onSubmit={this.handleFormSubmit}>
-          <div className="bx--grid">
-            <div className="bx--row">
-              <div className="bx--col">
-                <Select
-                  name="projectId"
-                  labelText={i18n.t('manageUsers.assign.projectName')}
-                  value={this.state.projectId}
-                  onChange={this.handleChanges}
-                >
-                  {projectList}
-                </Select>
-              </div>
+        return (
+            <div>
+                <Form onSubmit={this.handleFormSubmit}>
+                    <div className="bx--grid">
+                        <div className="bx--row">
+                            <div className="bx--col">
+                                <Select
+                                    name="projectId"
+                                    labelText={i18n.t('manageUsers.assign.projectName')}
+                                    value={this.state.projectId}
+                                    onChange={this.handleChanges}
+                                >
+                                    {projectList}
+                                </Select>
+                            </div>
+                        </div>
+                        <div className="bx--row">
+                            <div className="bx--col">
+                                <Select
+                                    name="assignUser"
+                                    labelText={i18n.t('manageUsers.assign.assignUser')}
+                                    value={this.state.assignUser}
+                                    onChange={this.handleChanges}
+                                >
+                                    {userList}
+                                </Select>
+                            </div>
+                        </div>
+                        <Button kind="primary" tabIndex={0} type="submit">
+                            {' '}
+                            {i18n.t('buttons.submit')}{' '}
+                        </Button>
+                    </div>
+                </Form>
             </div>
-            <div className="bx--row">
-              <div className="bx--col">
-                <Select
-                  name="assignUser"
-                  labelText={i18n.t('manageUsers.assign.assignUser')}
-                  value={this.state.assignUser}
-                  onChange={this.handleChanges}
-                >
-                  {userList}
-                </Select>
-              </div>
-            </div>
-            <Button kind="primary" tabIndex={0} type="submit">
-              {' '}
-              {i18n.t('buttons.submit')}{' '}
-            </Button>
-          </div>
-        </Form>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default withKeycloak(AssignUser);
