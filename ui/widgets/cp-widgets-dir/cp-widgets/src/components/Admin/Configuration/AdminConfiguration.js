44b452feef2ed09f5a4e1c5cf08664dfe8ffdcb9
jordengerovac <jorden.gerovac@veriday.com>
2021-03-25 10:43:53 -0400

    updating admin config component

--- ui/widgets/cp-widgets-dir/cp-widgets/src/components/Admin/Configuration/AdminConfiguration.js
+++ ui/widgets/cp-widgets-dir/cp-widgets/src/components/Admin/Configuration/AdminConfiguration.js
@@ -4,44 +4,51 @@ import { Accordion, AccordionItem, Tile } from 'carbon-components-react';
 import TicketingSystem from './TicketingSystem';
 import ProductVersion from './ProductVersion';
 
-const adminConfig = [
-  {
-    label: (
-      <div>
-        <p className="title">{i18n.t('adminConfig.integrationTicketingSystem.title')}</p>
-        <p className="desc">{i18n.t('adminConfig.integrationTicketingSystem.desc')}</p>
-      </div>
-    ),
-    content: <TicketingSystem />
-  },
-  {
-    label: (
-      <div>
-        <p className="title">{i18n.t('adminConfig.manageProductVersion.title')}</p>
-        <p className="desc">{i18n.t('adminConfig.manageProductVersion.desc')}</p>
-      </div>
-    ),
-    content: <ProductVersion />
+class AdminConfiguration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.adminConfig = [
+      {
+        label: (
+          <div>
+            <p className="title">{i18n.t('adminConfig.integrationTicketingSystem.title')}</p>
+            <p className="desc">{i18n.t('adminConfig.integrationTicketingSystem.desc')}</p>
+          </div>
+        ),
+        content: <TicketingSystem serviceUrl={this.props.serviceUrl} />
+      },
+      {
+        label: (
+          <div>
+            <p className="title">{i18n.t('adminConfig.manageProductVersion.title')}</p>
+            <p className="desc">{i18n.t('adminConfig.manageProductVersion.desc')}</p>
+          </div>
+        ),
+        content: <ProductVersion serviceUrl={this.props.serviceUrl} />
+      }
+    ];
   }
-];
 
-const AdminConfiguration = (props) => (
-  <div>
-    <h3 className="pageTitle">Entando Admin View</h3>
-    <div className="form-container">
-      <Tile>
-        <p className="title">{i18n.t('adminConfig.title')}</p>
-        <p class="desc">{i18n.t('adminConfig.desc')}</p>
-      </Tile>
-      <Accordion>
-        {adminConfig.map((item, index) => (
-          <AccordionItem key={index.toString()} index={index} title={item.label} description={item.description}>
-            <p>{item.content}</p>
-          </AccordionItem>
-        ))}
-      </Accordion>
+render() {
+  return(
+    <div>
+      <h3 className="pageTitle">Entando Admin View</h3>
+      <div className="form-container">
+        <Tile>
+          <p className="title">{i18n.t('adminConfig.title')}</p>
+          <p class="desc">{i18n.t('adminConfig.desc')}</p>
+        </Tile>
+        <Accordion>
+          {this.adminConfig.map((item, index) => (
+            <AccordionItem key={index.toString()} index={index} title={item.label} description={item.description}>
+              <p>{item.content}</p>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
-  </div>
-);
+  )
+}
 
+}
 export default AdminConfiguration;
