b6c71118f6d3b9bccb1178774cb21174d73a559e
Nathan Shaw <n.shaw@entando.com>
2021-05-06 22:26:46 -0400

    CP-73 Adjust generated repository changes

--- src/main/java/com/mycompany/myapp/repository/ProjectRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ProjectRepository.java
@@ -5,21 +5,32 @@ import com.mycompany.myapp.domain.Project;
 import com.mycompany.myapp.domain.Customer;
 
 import com.mycompany.myapp.domain.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Project entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Iterable<Project> findByCustomer(Customer customer);
-    List<Project> findByName(String name);
 
-    @Override
-    @Query
-    public List<Project> findAll();
+    @Query(value = "select distinct project from Project project left join fetch project.users",
+        countQuery = "select count(distinct project) from Project project")
+    Page<Project> findAllWithEagerRelationships(Pageable pageable);
+
+    @Query("select distinct project from Project project left join fetch project.users")
+    List<Project> findAllWithEagerRelationships();
+
+    @Query("select project from Project project left join fetch project.users where project.id =:id")
+    Optional<Project> findOneWithEagerRelationships(@Param("id") Long id);
+
+    Iterable<Project> findByCustomer(Customer customer);
+
+    List<Project> findByName(String name);
 }
